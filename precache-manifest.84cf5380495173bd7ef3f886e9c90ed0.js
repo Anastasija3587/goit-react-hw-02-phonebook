117274eca3295e1726bd908afc89b2ce4faef9bd
Anastasija3587 <Anastasija3587gmail.com>
2019-12-21 17:55:59 +0200

    Updates

---
+++ precache-manifest.84cf5380495173bd7ef3f886e9c90ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3b3d749b2999d318b6188afe0c1583",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "e59804fc8b25cab86665",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.38221ce1.chunk.css"
+  },
+  {
+    "revision": "8c64f98d510d0a902a69",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.315c70da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.315c70da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e59804fc8b25cab86665",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.046c65e0.chunk.js"
+  },
+  {
+    "revision": "c3f8367271860c7b9688",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.cab97b21.js"
+  }
+]);
